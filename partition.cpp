5600ffcdf3e0ea40bde0e498e0c3628488dc1d5c
Egor Pilipenko <ctapchuk@gmail.com>
2025-03-31 12:15:27 +0700

    partition.cpp: Add support for "wiping" emmc partitions Use blkdiscard for this
    Signed-off-by: Egor Pilipenko <ctapchuk@gmail.com>

--- partition.cpp
+++ partition.cpp
@@ -1892,6 +1892,8 @@ bool TWPartition::Wipe(string New_File_System) {
 			wiped = Wipe_F2FS();
 		else if (New_File_System == "vfat")
 			wiped = Wipe_FAT();
+		else if (New_File_System == "emmc")
+			wiped = BlkDiscard();
 		else {
 			LOGERR("Unable to wipe '%s' -- unknown file system '%s'\n", Mount_Point.c_str(), New_File_System.c_str());
 			return false;
