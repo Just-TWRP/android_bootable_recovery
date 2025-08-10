e42e3e3df5cac95d1fdb3394efc5515fa7c41c54
sekaiacg <sekaiacg@gmail.com>
2025-08-10 21:00:54 +0800

    recovery: Add disable AVB2.0 in advanced options
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- partitions.hpp
+++ partitions.hpp
@@ -410,6 +410,7 @@ public:
 	void Unlock_Block_Partitions();                                           // Unlock all block devices after update_engine runs
 	bool Unmap_Super_Devices();                                               // Unmap super devices in TWRP
 	bool Check_Pending_Merges();                                              // Check and run pending merges on data for VAB devices
+    bool Disable_AVB2(bool Display_Info);                                     // Disable AVB 2.0
 
 private:
 	void Setup_Settings_Storage_Partition(TWPartition* Part);                 // Sets up settings storage
